9e54298d97ac75fe6c4d8d684a4b7f4d3ca85189
Emad <65307894+emadalramimi@users.noreply.github.com>
2024-03-17 14:08:43 +0100

    Detection using camera

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import cv2
 
-img = cv2.imread('person.png')
+# img = cv2.imread('person.png')
+
+camera = cv2.VideoCapture(0)
+camera.set(3, 740)
+camera.set(4, 580)
 
 classNames = []
-classFile = 'coco.names'
+classFile = 'things.names'
 
 with open(classFile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
@@ -18,14 +22,15 @@ net.setInputScale(1.0/127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
-classIds, confs, bbox = net.detect(img, confThreshold=0.5)
+while True:
+    success, img = camera.read()
+    classIds, confs, bbox = net.detect(img, confThreshold=0.5)
 
-print(classIds, bbox)
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+            cv2.putText(img, classNames[classId-1].upper(), (box[0]+10, box[1]+30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(img, str(round(confidence*100, 2)), (box[0]+200, box[1]+30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
 
-for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-    cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
-    cv2.putText(img, classNames[classId-1].upper(), (box[0]+10, box[1]+30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-    cv2.putText(img, str(round(confidence*100, 2)), (box[0]+200, box[1]+30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-
-cv2.imshow('Output',img)
-cv2.waitKey(0)
+    cv2.imshow('Output',img)
+    cv2.waitKey(1)
